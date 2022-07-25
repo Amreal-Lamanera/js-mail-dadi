fbea7b669138806fdb211271d84194b494b1261b
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-25 14:32:59 +0200

    add dadi ex

---
+++ dadi-ex/README.md
@@ -0,0 +1,9 @@
+# Esercizio Gioco dei dadi
+
+- Genera un numero per il giocatore
+- Genera un numero per il computer
+- SE playerNum > pcNum
+    - Allora giocatore vince
+- ALTRIMENTI SE playerNum < pcNum
+    - Allora computer vince
+- ALTRIMENTI pareggio
---
+++ dadi-ex/js/app.js
@@ -0,0 +1,33 @@
+// console.log('js ok');
+
+const btnElement = document.querySelector('button');
+const yourResult = document.getElementById('your-result');
+const myResult = document.getElementById('my-result');
+const winner = document.getElementById('winner');
+
+// console.log(btnElement);
+btnElement.addEventListener('click', function () {
+    // console.log('js ok2');
+    // - Genera un numero per il giocatore
+    const playerNum = Math.floor(Math.random() * (7 - 1)) + 1;
+    yourResult.innerHTML = 'Hai fatto: ' + playerNum;
+    // console.log(playerNum);
+    // - Genera un numero per il computer
+    const pcNum = Math.floor(Math.random() * (7 - 1)) + 1;
+    myResult.innerHTML = 'Io ho fatto: ' + pcNum;
+
+    // - SE playerNum > pcNum
+    if (playerNum > pcNum) {
+        // - allora giocatore vince
+        winner.innerHTML = 'Hai vinto!';
+    } // - ALTRIMENTI SE playerNum > pcNum 
+    else if (playerNum < pcNum) {
+        // - Allora computer vince
+        winner.innerHTML = 'Hai perso!';
+    } // - ALTRIMENTI
+    else {
+        // - Pareggio
+        winner.innerHTML = 'Pareggio!';
+    }
+})
+
